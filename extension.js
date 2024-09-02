740cdf91444350cef8dcdf71c8cf46ee4031488a
deJames-13 <de.james013@gmail.com>
2024-09-02 20:43:36 +0800

    waowie colors

--- extension.js
+++ extension.js
@@ -33,16 +33,55 @@ function activate(context) {
 
 
       const newColors = {
-        "terminalCursor.background": colors['cursor'],
-        "terminalCursor.foreground": colors['cursor'],
-        "statusBar.background": colors['color13'],
-        "statusBar.noFolderBackground": colors['color0'],
-        "statusBar.debuggingBackground": colors['color14'],
-        "statusBar.debuggingForeground": colors['foreground'],
-        "titleBar.activeBackground": colors['color1'],
-        "titleBar.activeForeground": colors['foreground'],
-        "titleBar.inactiveBackground": colors['color0'],
-        "titleBar.inactiveForeground": colors['foreground']
+        "terminalCursor.background": colors["color1"], // cursor
+        "statusBar.background": colors["color1"], // color1
+        "scrollbarSlider.background": colors["color5"] + '69', // color5 with 69
+
+        "badge.background": colors["color6"], // color6
+        "activityBarBadge.background": colors["color6"], // color6
+        "button.background": colors["color1"], // color1
+        "breadcrumb.focusForeground": colors["color6"], // color6
+        "list.activeSelectionBackground": colors["color1"], // color1
+        "list.inactiveSelectionBackground": colors["color1"], // color1
+        "list.focusAndSelectionOutline": colors["color5"] + '69', // color5 with 69
+        "focusBorder": colors["color6"], // color6
+        
+        "statusBar.noFolderBackground": colors["color0"], // color0
+        "statusBar.debuggingBackground": colors["color6"], // color6
+        "titleBar.inactiveBackground": colors["color5"], // color5
+
+        "titleBar.activeBackground": colors["color2"], // color2
+        "activityBar.activeBackground": colors["color1"] + '69', // color1 with 69
+        "tab.activeBackground": colors["color1"] + '69', // color1 with 69
+        "minimapSlider.activeBackground": colors["color5"] + '69', // color5 with 69
+        "scrollbarSlider.activeBackground": colors["color5"] + '69', // color5 with 69
+
+        "titleBar.border": colors["color5"], // color5,
+        "tab.activeBorder": colors["color5"], // color5
+        // "sideBar.border": "//#6C4A97", // color5
+        "activityBar.activeBorder": colors["color5"], // color5
+
+
+        "terminalCursor.foreground": colors["cursor"], // cursor
+        "statusBar.debuggingForeground": colors["cursor"], // cursor
+        "titleBar.inactiveForeground": colors["cursor"], // cursor
+
+        // HOVER
+        "tab.hoverBackground": colors["color5"] + '69', // color5 with 69
+        "extensionButton.hoverBackground": colors["color5"] + '69', // color5 with 69
+        "list.hoverBackground": colors["color5"] + '69', // color5  with 69
+        "toolbar.hoverOutline": colors["color5"], // color5
+        "minimapSlider.hoverBackground": colors["color5"] + '69', // color5 with 69
+        "tab.hoverBorder": colors["color5"], // color5
+        "button.hoverBackground": colors["color5"] + '69', // color5 with 69
+        "sash.hoverBorder": colors["color5"] + '69', // color5 with 69
+        "settings.rowHoverBackground": colors["color5"] + '69', // color5 with 69
+        "button.secondaryHoverBackground": colors["color5"] + '69', // color5 with 69
+        "scrollbarSlider.hoverBackground": colors["color5"] + '69', // color5 with 69
+        "editor.wordHighlightBackground":  colors["color5"] + '69', // color5 with 69
+        "editor.lineHighlightBackground":  colors["color5"] + '69', // color5 with 69
+        "editor.selectionHighlightBackground": colors["color5"] + '69', // color5 with 69
+        "editor.selectionBackground": colors["color5"] + '69', // color5 with 69
       }
 
       const currentColors = config.get('workbench.colorCustomizations') || {};
