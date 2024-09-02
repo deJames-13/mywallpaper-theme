cf89fa203b78f328fd40f81f5498f96f94992fce
deJames-13 <de.james013@gmail.com>
2024-09-02 19:24:19 +0800

    more config

--- extension.js
+++ extension.js
@@ -6,11 +6,9 @@ function activate(context) {
   let disposable = vscode.commands.registerCommand('extension.changeColorTheme', async () => {
     const config = vscode.workspace.getConfiguration();
     
-    // Path to the CSS file containing the color scheme
-    const cssFilePath = path.join(process.env.HOME || '', '.cache/wal/colors-waybar.css');
+    const cssFilePath = config.get('wallpaperTheme.cssFilePath').replace("${env:HOME}", process.env.HOME);
     console.log(`Reading CSS file from: ${cssFilePath}`);
 
-    // Read the CSS file
     fs.readFile(cssFilePath, 'utf8', (err, data) => {
       if (err) {
         vscode.window.showErrorMessage('Error reading CSS file');
@@ -22,7 +20,6 @@ function activate(context) {
 
       const colors = {};
 
-      // Manually parse the CSS file to extract color definitions
       const lines = data.split('\n');
       lines.forEach(line => {
         const match = line.match(/@define-color\s+(\w+)\s+(#[0-9a-fA-F]{6});/);
@@ -34,27 +31,24 @@ function activate(context) {
 
       console.log('Extracted colors:', colors);
 
-      // Define your dynamic colors here based on the color scheme
-      const dynamicColors = {
-        "workbench.colorCustomizations": {
-          "terminal.background": colors['color0'],
-          "terminal.foreground": colors['foreground'],
-          "terminalCursor.background": colors['cursor'],
-          "terminalCursor.foreground": colors['cursor'],
-          "statusBar.background": colors['color1'],
-          "statusBar.foreground": colors['foreground'],
-          "statusBar.noFolderBackground": colors['color2'],
-          "statusBar.noFolderForeground": colors['foreground'],
-          "statusBar.debuggingBackground": colors['color3'],
-          "statusBar.debuggingForeground": colors['foreground'],
-          "titleBar.activeBackground": colors['color4'],
-          "titleBar.activeForeground": colors['foreground'],
-          "titleBar.inactiveBackground": colors['color5'],
-          "titleBar.inactiveForeground": colors['foreground'],
-        }
-      };
 
-      // Update the settings.json with dynamic colors
+      const newColors = {
+        "terminalCursor.background": colors['cursor'],
+        "terminalCursor.foreground": colors['cursor'],
+        "statusBar.background": colors['color1'],
+        "statusBar.noFolderBackground": colors['color2'],
+        "statusBar.debuggingBackground": colors['color3'],
+        "statusBar.debuggingForeground": colors['foreground'],
+        "titleBar.activeBackground": colors['color4'],
+        "titleBar.activeForeground": colors['foreground'],
+        "titleBar.inactiveBackground": colors['color5'],
+        "titleBar.inactiveForeground": colors['foreground']
+      }
+
+      const currentColors = config.get('workbench.colorCustomizations') || {};
+
+      const dynamicColors = { ...currentColors, ...newColors };
+
       config.update('workbench.colorCustomizations', dynamicColors['workbench.colorCustomizations'], vscode.ConfigurationTarget.Global)
         .then(() => {
           vscode.window.showInformationMessage('Color theme updated!');
