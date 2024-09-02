3d9b28690ca14587ebb1869075e269052850ac2e
deJames-13 <de.james013@gmail.com>
2024-09-02 22:03:11 +0800

    test

--- extension.js
+++ extension.js
@@ -1,13 +1,13 @@
 
 const vscode = require('vscode');
-const changeColorTheme = require('./commands/changeColorTheme.js');
-const removeColorTheme = require('./commands/removeColorTheme.js');
+const changeColorTheme = require('./src/changeColorTheme.js');
+const removeColorTheme = require('./src/removeColorTheme.js');
 
 function activate(context) {
-  let changeCommand = vscode.commands.registerCommand('my-wallpaper-theme.changeColorTheme', changeColorTheme);
+  let changeCommand = vscode.commands.registerCommand('extension.changeColorTheme', changeColorTheme);
   context.subscriptions.push(changeCommand);
 
-  let removeCommand = vscode.commands.registerCommand('my-wallpaper-theme.removeColorTheme', removeColorTheme)
+  let removeCommand = vscode.commands.registerCommand('extension.removeColorTheme', removeColorTheme)
   context.subscriptions.push(removeCommand);
 }
 
